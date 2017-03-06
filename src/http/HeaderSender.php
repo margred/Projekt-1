7feca9a1e310ed1202e2906d47518a4a0276f469
Sönke Lange <s.lange@mytaxi.com>
2017-03-06 23:58:49 +0100

    Add implementation of ResponseSender

---
+++ src/http/HeaderSender.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: s.lange
+ * Date: 06.03.17
+ * Time: 23:50
+ */
+
+namespace HAWMS\http;
+
+
+class HeaderSender
+{
+    public function send(string $headerString)
+    {
+    }
+}
--- src/http/Response.php
+++ src/http/Response.php
@@ -4,5 +4,16 @@ namespace HAWMS\http;
 
 interface Response
 {
+    /**
+     * @return int
+     */
+    public function getStatusCode();
 
+
+    /**
+     * @return string
+     */
+    public function getStatusReasonPhrase();
+
+    public function flush();
 }
