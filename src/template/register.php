22c00bb22644841d58a841601a4a92fdda183db3
Sönke Lange <s.lange@mytaxi.com>
2017-03-12 20:05:20 +0100

    Add a topbar

---
+++ src/template/register.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link href="./css/style.css" rel="stylesheet">
+</head>
+<div class="layout">
+    <div class="layout__topbar topbar">
+        <div class="topbar__logo">Projektname</div>
+        <nav class="navigation">
+            <a href="signup" class="navigation__link">Sign up</a>
+            <a href="login" class="navigation__link">Login</a>
+        </nav>
+    </div>
+    <div class="layout__content">
+    </div>
+</div>
+</body>
+</html>
