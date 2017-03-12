13dfa60ca530b9af407d27c1318fef6ef9a6bd61
Sönke Lange <s.lange@mytaxi.com>
2017-03-12 15:07:33 +0100

    Replace SampleFilter with DispatcherFilter in Application filter chain

--- src/Application.php
+++ src/Application.php
@@ -2,9 +2,14 @@
 
 namespace HAWMS;
 
+use HAWMS\controller\UserRegistrationController;
+use HAWMS\http\Dispatcher;
+use HAWMS\http\DispatcherFilter;
 use HAWMS\http\FilterChain;
 use HAWMS\http\Request;
 use HAWMS\http\Response;
+use HAWMS\view\ViewRenderer;
+use HAWMS\view\ViewResolver;
 
 class Application
 {
@@ -15,8 +20,12 @@ class Application
      */
     public function __construct()
     {
+        $viewResolver = new ViewResolver(__DIR__ . '/template');
+        $viewRenderer = new ViewRenderer();
+        $userRegistrationController = new UserRegistrationController();
+        $dispatch = new Dispatcher($viewResolver, $viewRenderer, $userRegistrationController);
         $this->filterChain = new FilterChain();
-        $this->filterChain->addFilter(new SampleFilter());
+        $this->filterChain->addFilter(new DispatcherFilter($dispatch));
     }
 
     /**
