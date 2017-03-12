866f3196c2a9ed484b8068e52c7b8f61e279e13a
Sönke Lange <s.lange@mytaxi.com>
2017-03-12 18:06:23 +0100

    Add Gulp task to run local server with browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var connect = require('gulp-connect-php');
+var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function () {
     gulp.src('scss/**/*.scss')
@@ -7,6 +9,21 @@ gulp.task('sass', function () {
         .pipe(gulp.dest('./public/css/'))
 });
 
-gulp.task('default', function () {
-    gulp.watch('scss/**/*.scss', ['sass']);
+gulp.task('serve', ['sass'], function () {
+    var port = 8000;
+    var config = {
+        base: 'public',
+        port: port,
+        keepalive: true
+    };
+    connect.server(config, function (){
+        browserSync.init({
+            proxy: '127.0.0.1:' + port
+        });
+    });
+});
+
+gulp.task('default', ['serve'], function () {
+    gulp.watch('scss/**/*.scss', ['sass', browserSync.reload]);
+    gulp.watch('src/**/*.php', [browserSync.reload]);
 });
