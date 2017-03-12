63480fea8088d68f9c075cbffd5bb6bfc7399c4b
Sönke Lange <s.lange@mytaxi.com>
2017-03-12 14:22:32 +0100

    Add implementation of ViewRenderer

--- src/view/ViewRenderer.php
+++ src/view/ViewRenderer.php
@@ -6,5 +6,11 @@ class ViewRenderer
 {
     public function render(View $view, array $model = [])
     {
+        extract($model);
+        ob_start();
+
+        include $view->getPath();
+
+        return ob_get_clean();
     }
 }
---
+++ test/view/ViewRendererTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use HAWMS\view\ViewRenderer;
+
+class ViewRendererTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @var ViewRenderer
+     */
+    private $viewRenderer;
+
+    protected function setUp()
+    {
+        $this->viewRenderer = new ViewRenderer();
+    }
+
+    public function testShouldReturnViewContent()
+    {
+        $view = new \HAWMS\view\View(dirname(__DIR__) . '/template/test.php');
+        $model = [
+            'foo' => 'bar'
+        ];
+        $content = $this->viewRenderer->render($view, $model);
+
+        $this->assertEquals("<h1>bar</h1>\n", $content);
+    }
+}
