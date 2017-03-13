44486842804eefff09d4cba7bebe3720b795c019
Sönke Lange <s.lange@mytaxi.com>
2017-03-13 22:15:40 +0100

    Add implementation of UniversityRepository

--- src/repository/UniversityRepository.php
+++ src/repository/UniversityRepository.php
@@ -2,9 +2,27 @@
 
 namespace HAWMS\repository;
 
+use PDO;
+
 class UniversityRepository
 {
+    /**
+     * @var PDO
+     */
+    private $connection;
+
+    /**
+     * UniversityRepository constructor.
+     */
+    public function __construct(\PDO $connection)
+    {
+        $this->connection = $connection;
+    }
+
     public function findAll()
     {
+        $result = $this->connection->query('SELECT * FROM universities');
+        $result->setFetchMode(PDO::FETCH_CLASS, 'HAWMS\model\University');
+        return $result->fetchAll();
     }
 }
