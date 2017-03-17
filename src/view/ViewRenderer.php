98cca3e5c336580d75971f93f5d0023c692c7995
Sönke Lange <s.lange@mytaxi.com>
2017-03-17 16:39:59 +0100

    Change ViewRenderer to wrap view in layout if layoutPath is present

--- src/view/ViewRenderer.php
+++ src/view/ViewRenderer.php
@@ -4,7 +4,29 @@ namespace HAWMS\view;
 
 class ViewRenderer
 {
+    private $layoutPath;
+    private $content;
+
     public function render(View $view, array $model = [])
+    {
+        $this->content = $this->renderView($view, $model);
+        if ($this->layoutPath) {
+            return $this->renderLayout();
+        }
+        return $this->content;
+    }
+
+    public function setLayoutPath($layoutPath)
+    {
+        $this->layoutPath = $layoutPath;
+    }
+
+    /**
+     * @param View $view
+     * @param array $model
+     * @return string
+     */
+    private function renderView(View $view, array $model): string
     {
         extract($model);
         ob_start();
@@ -12,5 +34,19 @@ class ViewRenderer
         include $view->getPath();
 
         return ob_get_clean();
+    }
+
+    private function renderLayout()
+    {
+        ob_start();
+
+        include $this->layoutPath;
+
+        return ob_get_clean();
+    }
+
+    private function content()
+    {
+        return $this->content;
     }
 }
